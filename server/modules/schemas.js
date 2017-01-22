259c30c7805e59ed2cffba8802adb6bc38e7ee47
Frank Matranga <thefrankmatranga@gmail.com>
2017-01-22 00:03:59 -0500

    Schema updates

--- server/modules/schemas.js
+++ server/modules/schemas.js
@@ -4,12 +4,26 @@ const Schema = mongoose.Schema;
 // SCHEMA NAMES MUST BE IN PascalCase
 module.exports = {
     Person: {
-        
+        firstName: String,
+        lastName: String,
+        title: String,
+        relation: String,
+        description: String,
+        levels: {
+
+        },
+        tags: { type: [String], default: [] }
     },
     Contemplation: {
-
+        _personId: Schema.Types.ObjectId,
+        _eventId: Schema.Types.ObjectId,
+        date: { type: Date, default: Date.now },
+        title: String,
+        text: String
     },
     Event: {
-        
+        date: { type: Date, default: Date.now },
+        title: String,
+        location: String
     }
 };
